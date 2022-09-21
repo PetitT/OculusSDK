28ca5a3b1e5e57527cc81ab696845fa573465357
Thibaut <xcthib@hotmail.com>
2022-09-21 22:19:49 +0200

    Update sdk version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# OculusSDK
+# Oculus SDK Plugin for Unity
+
+This is just the Oculus SDK
+
+## Documentation
+
+https://developer.oculus.com/documentation/unity/unity-overview/
--- Runtime/Oculus/Platform/Scripts/MicrophoneInput.cs
+++ Runtime/Oculus/Platform/Scripts/MicrophoneInput.cs
@@ -4,70 +4,70 @@
 // NOTE for android developers: The existence of UnityEngine.Microphone causes Unity to insert the 
 // android.permission.RECORD_AUDIO permission into the AndroidManifest.xml generated at build time
 
-//#if OVR_PLATFORM_USE_MICROPHONE
-//using UnityEngine;
-//using System.Collections.Generic;
+#if OVR_PLATFORM_USE_MICROPHONE
+using UnityEngine;
+using System.Collections.Generic;
 
-//namespace Oculus.Platform
-//{
+namespace Oculus.Platform
+{
 
-//  public class MicrophoneInput : IMicrophone
-//  {
-//    AudioClip microphoneClip;
-//    int lastMicrophoneSample;
-//    int micBufferSizeSamples;
+  public class MicrophoneInput : IMicrophone
+  {
+    AudioClip microphoneClip;
+    int lastMicrophoneSample;
+    int micBufferSizeSamples;
 
-//    private Dictionary<int, float[]> micSampleBuffers;
+    private Dictionary<int, float[]> micSampleBuffers;
 
-//    public MicrophoneInput()
-//    {
-//      int bufferLenSeconds = 1; //minimum size unity allows
-//      int inputFreq = 48000; //this frequency is fixed throughout the voip system atm
-//      microphoneClip = Microphone.Start(null, true, bufferLenSeconds, inputFreq);
-//      micBufferSizeSamples = bufferLenSeconds * inputFreq;
+    public MicrophoneInput()
+    {
+      int bufferLenSeconds = 1; //minimum size unity allows
+      int inputFreq = 48000; //this frequency is fixed throughout the voip system atm
+      microphoneClip = Microphone.Start(null, true, bufferLenSeconds, inputFreq);
+      micBufferSizeSamples = bufferLenSeconds * inputFreq;
 
-//      micSampleBuffers = new Dictionary<int, float[]>();
-//    }
+      micSampleBuffers = new Dictionary<int, float[]>();
+    }
 
-//    public void Start()
-//    {
+    public void Start()
+    {
 
-//    }
+    }
 
-//    public void Stop()
-//    {
-//    }
+    public void Stop()
+    {
+    }
 
-//    public float[] Update()
-//    {
-//      int pos = Microphone.GetPosition(null);
-//      int copySize = 0;
-//      if (pos < lastMicrophoneSample)
-//      {
-//        int endOfBufferSize = micBufferSizeSamples - lastMicrophoneSample;
-//        copySize = endOfBufferSize + pos;
-//      }
-//      else
-//      {
-//        copySize = pos - lastMicrophoneSample;
-//      }
+    public float[] Update()
+    {
+      int pos = Microphone.GetPosition(null);
+      int copySize = 0;
+      if (pos < lastMicrophoneSample)
+      {
+        int endOfBufferSize = micBufferSizeSamples - lastMicrophoneSample;
+        copySize = endOfBufferSize + pos;
+      }
+      else
+      {
+        copySize = pos - lastMicrophoneSample;
+      }
 
-//      if (copySize == 0) {
-//        return null;
-//      }
+      if (copySize == 0) {
+        return null;
+      }
 
-//      float[] samples;
-//      if (!micSampleBuffers.TryGetValue(copySize, out samples))
-//      {
-//        samples = new float[copySize];
-//        micSampleBuffers[copySize] = samples;
-//      }
+      float[] samples;
+      if (!micSampleBuffers.TryGetValue(copySize, out samples))
+      {
+        samples = new float[copySize];
+        micSampleBuffers[copySize] = samples;
+      }
 
-//      microphoneClip.GetData(samples, lastMicrophoneSample);
-//      lastMicrophoneSample = pos;
-//      return samples;
+      microphoneClip.GetData(samples, lastMicrophoneSample);
+      lastMicrophoneSample = pos;
+      return samples;
 
-//    }
-//  }
-//}
-//#endif
+    }
+  }
+}
+#endif
